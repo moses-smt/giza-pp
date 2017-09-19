f3d27bc844edcc1963a4e3340820313051da25e5
Andrew Yuan <solinight@outlook.com>
2017-09-19 17:03:32 +0800

    Update model3.cpp

--- GIZA++-v2/model3.cpp
+++ GIZA++-v2/model3.cpp
@@ -277,7 +277,8 @@ model3::model3(model2& m2) :
 {}
 */
   string trainingString;
-  trainingString+=(h?'H':'3');
+  trainingString+=(h?'H':'3');  //这里我们一开始把h初始化为指向0(NULL)的指针，所以这里等价于trainingString+='3'
+  //这里的noIterationModel[3,4,5,6]对应的是我们的全局变量Model[3,4,5,6]_Iterations，即迭代次数，易知下面的逻辑是迭代几次，concatenate几个'[3,4,5,6]'
   for(int i=0;i<noIterationsModel3;++i) trainingString+='3';
   for(int i=0;i<noIterationsModel4;++i) trainingString+='4';
   for(int i=0;i<noIterationsModel5;++i) trainingString+='5';

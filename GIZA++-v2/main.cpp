e33d79fcd8e4857b9616b1f3418608efd28b6392
Andrew Yuan <solinight@outlook.com>
2017-09-27 11:52:11 +0800

    Update main.cpp

--- GIZA++-v2/main.cpp
+++ GIZA++-v2/main.cpp
@@ -627,7 +627,7 @@ class Dictionary{
   // 下面参数中的tTable是在之前定义的：tmodel<COUNT, PROB> tTable; 还没有初始化。当然如果我们有BINARY_SEARCH_FOR_TTABLE的话，
   // 那么就变成了tmodel<COUNT, PROB> tTable(CoocurrenceFile); 定义且初始化了。
    model1 m1(CorpusFilename.c_str(), eTrainVcbList, fTrainVcbList,tTable,trainPerp, 
-	    *corpus,&testPerp, testCorpus, 
+	    *corpus,&testPerp, testCorpus, //这里的testPerp是前面定义的全局变量所以它必不为NULL,而testCorpus则取决于我们在./GIZA++时是否传入-tc参数。
 	    trainViterbiPerp, &testViterbiPerp); //这里传入的是*corpus和testcorpus,一个sentenceHandler一个是指向sentenceHandler的指针。
    amodel<PROB>  aTable(false); // typedef float PROB ;
    amodel<COUNT> aCountTable(false); //typedef float COUNT ;

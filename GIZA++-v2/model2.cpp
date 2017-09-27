2d5cd99f30ecfab666dd96226f8e1b0fe6750f42
Andrew Yuan <solinight@outlook.com>
2017-09-27 11:22:54 +0800

    Update model2.cpp

--- GIZA++-v2/model2.cpp
+++ GIZA++-v2/model2.cpp
@@ -217,6 +217,7 @@ void model2::em_loop(Perplexity& perp, sentenceHandler& sHandler1,
     sHandler1.setProbOfSentence(sent,cross_entropy);
     perp.addFactor(cross_entropy, so, l, m,1);
     viterbi_perp.addFactor(log(viterbi_score), so, l, m,1);
+    //同理这里的if条件也是不满足的
     if (dump_alignment||(FEWDUMPS&&sent.sentenceNo<1000) )
       printAlignToFile(es, fs, Elist.getVocabList(), Flist.getVocabList(), of2, viterbi_alignment, sent.sentenceNo, viterbi_score);
     addAL(viterbi_alignment,sent.sentenceNo,l);
